6ef330ee781a3a8be4cd9c38fd394a75254e8006
Kamran Amini <kam.cpp@gmail.com>
2016-04-18 01:51:57 +0430

    Generated java sources were added.

---
+++ hot/gate-java/hotgen/server/GateMonitorImpl.java
@@ -0,0 +1,11 @@
+package ir.ntnaeem.gate.hotgen.server;
+
+import ir.ntnaeem.gate.hotgen.*;
+
+class GateMonitorImpl extends AbstractGateMonitorService {
+    @Override
+    public String getCurrentStat() {
+        return null;
+    }
+    //implementation
+}
---
+++ hot/gate-java/hotgen/server/GateTestImpl.java
@@ -0,0 +1,11 @@
+package ir.ntnaeem.gate.hotgen.server;
+
+import ir.ntnaeem.gate.hotgen.*;
+
+class GateTestImpl extends AbstractGateTestService {
+    @Override
+    public long enqueueAsIncomingMessage(Message message) {
+        return 0;
+    }
+    //implementation
+}
